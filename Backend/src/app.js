444fa9f9443fefbbf1f5efce6c109d3010f68221
Debojyoti <khanradebojyoti@gmail.com>
2026-09-21 17:05:48 +0530

    pre deploy build

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,14 +9,15 @@ const app = express();
  * Middleware
  */
 app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    }),
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	}),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static("./public"));
 
 /**
  * Routes
